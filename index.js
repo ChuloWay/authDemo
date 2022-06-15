364fcad53203bd9986984eecf585b7a401b8d650
Okoye Victor <okoyeebuka64@gmail.com>
2022-06-15 15:11:17 +0100

    Added-> Auth Logout, Auth Login Middleware, Refactored hash Code to Models

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ app.set('view engine', 'ejs')
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({secret:'naTestRunBeThisBoss'}));
+app.use(session({ secret: 'naTestRunBeThisBoss' }));
+
+const requireLogin= (req,res,next)=>{
+    if(!req.session.user_id){
+     return res.redirect('/login')
+    }
+    next();
+}
 
 
 mongoose.connect('mongodb://localhost:27017/authDemo', {
@@ -34,15 +41,10 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hashPw = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hashPw
-    });
+    const user = new User({ username,password });
     await user.save();
     req.session.user_id = user._id
-
-    res.redirect('/')
+    res.redirect('/secret')
 })
 
 app.get('/login', (req, res) => {
@@ -50,24 +52,25 @@ app.get('/login', (req, res) => {
 });
 
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPw = await bcrypt.compare(password, user.password);
-    if(validPw){
-        req.session.user_id = user._id
+    const foundUser = await User.findAndValidate(username,password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id
         res.redirect('/secret');
     }
-    else{
+    else {
         res.redirect('/login');
     }
 });
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect('/login')
-    }
-    res.send('This is my secret')
+app.post('/logout', async (req, res) => {
+    req.session.user_id = null;
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin,(req, res) => {
+    res.render('secret')
 })
 
 
